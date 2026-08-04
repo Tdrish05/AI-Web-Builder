edabeb5d1c8a7e1df22e0a762f083771c04d3fc6
Tdrish05 <dristideka9@gmail.com>
2026-08-04 21:41:03 +0530

    add loading state

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -2,9 +2,7 @@ import {Loader2Icon} from 'lucide-react'
 
 const Loading = () => {
   return (
-    <div className='h-screen flex items-center justify-center bg-white'>
-        <Loader2Icon size={26} className="animate-spin text-zinc-950"/>  
-    </div>
+    <div role="status" aria-label="loading" className='h-screen flex items-center justify-center bg=white'><Loader2Icon size={26} className="animate-spin text-zinc-950"/></div>
   )
 }
 
