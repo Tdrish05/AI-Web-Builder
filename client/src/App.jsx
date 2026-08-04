945af819200c3a8827f6acdc7028844034d03976
Tdrish05 <dristideka9@gmail.com>
2026-08-04 20:48:04 +0530

    setup layout and app context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { createContext, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { GuestLayout, AuthLayout } from "./pages/Layout";
+import AuthPage from "./pages/AuthPage";
+import Homepage from "./pages/HomePage";
+import BuilderPage from "./pages/BuilderPage";
+import PreviewPage from "./pages/PreviewPage";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Routes>
+      {/* Login Routes */}
+      <Route element={<GuestLayout />}>
+        <Route path="/login" element={<AuthPage mode="login" />} />
+        <Route path="/register" element={<AuthPage mode="register" />} />
+      </Route>
 
-export default App
+      {/* Protected Routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/builder/:id" element={<BuilderPage />} />
+        <Route path="/preview/:id" element={<PreviewPage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const AuthPage = () => {
+  return (
+    <div>
+      AuthPage
+    </div>
+  )
+}
+
+export default AuthPage
